a0e4294ba764ee835d7bae46a23692e100c1098c
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2024-04-09 19:27:38 +0200

    Deterministic version of CUDA forces and stresses kernels
    - Remove atomic operations in the forces and stress kernels.
    - Use template programming to minimize code duplication.
    
    Authors :
    - A. Sedova (ORNL)
    - M. Taillefumier (ETH Zurich / CSCS)

--- source/lib/src/gpu/prod_force.cu
+++ source/lib/src/gpu/prod_force.cu
@@ -43,61 +43,244 @@ __global__ void force_deriv_wrt_center_atom(FPTYPE* force,
   }
 }
 
-template <typename FPTYPE>
-__global__ void force_deriv_wrt_neighbors_a(FPTYPE* force,
-                                            const FPTYPE* net_deriv,
-                                            const FPTYPE* in_deriv,
-                                            const int* nlist,
-                                            const int nloc,
-                                            const int nall,
-                                            const int nnei) {
-  // idy -> nnei
-  const int_64 idx = blockIdx.x;
-  const unsigned int idy = blockIdx.y * blockDim.x + threadIdx.x;
-  const unsigned int idz = threadIdx.y;
-  const int ndescrpt = nnei * 4;
-  if (idy >= nnei) {
-    return;
+/* compute $\partial E_j / \partial D_{ji} \nabla_R_j D_{ji}$ */
+template <typename FPTYPE, bool radial_only_ = true>
+__inline__ __device__ void calculate_forces(
+    FPTYPE* forces__,
+    const FPTYPE* __restrict__ net_deriv__,
+    const FPTYPE* __restrict__ in_deriv__,
+    const int idx__) {
+  const int offset_j = idx__ * ((radial_only_) ? (1) : (4));
+  for (int idw = 0; idw < ((radial_only_) ? (1) : (4)); ++idw) {
+    const FPTYPE cst1 = net_deriv__[offset_j + idw];
+    forces__[0] += cst1 * in_deriv__[(offset_j + idw) * 3 + 0];
+    forces__[1] += cst1 * in_deriv__[(offset_j + idw) * 3 + 1];
+    forces__[2] += cst1 * in_deriv__[(offset_j + idw) * 3 + 2];
   }
-  // deriv wrt neighbors
-  int j_idx = nlist[idx * nnei + idy];
-  if (j_idx < 0) {
-    return;
-  }
-  FPTYPE force_tmp = (FPTYPE)0.;
-  for (int idw = 0; idw < 4; ++idw) {
-    force_tmp += net_deriv[idx * ndescrpt + idy * 4 + idw] *
-                 in_deriv[idx * ndescrpt * 3 + (idy * 4 + idw) * 3 + idz];
-  }
-  const int_64 kk = idx / nloc;  // frame index
-  atomicAdd(force + kk * nall * 3 + j_idx * 3 + idz, force_tmp);
 }
 
-template <typename FPTYPE>
-__global__ void force_deriv_wrt_neighbors_r(FPTYPE* force,
-                                            const FPTYPE* net_deriv,
-                                            const FPTYPE* in_deriv,
-                                            const int* nlist,
-                                            const int nloc,
-                                            const int nall,
-                                            const int nnei) {
-  // idy -> nnei
-  const int_64 idx = blockIdx.x;
-  const unsigned int idy = blockIdx.y * blockDim.x + threadIdx.x;
-  const unsigned int idz = threadIdx.y;
-  const int ndescrpt = nnei * 1;
-  if (idy >= nnei) {
+/*
+  The original code computes
+
+  f_j += \sum_{k neighbor of j} \delta_{ik} f_jk
+
+  Parallelization is over the i index not the j index which means that several
+  threads can update the force of the same atom j. It requires an atomic
+  operation to do so.
+
+  Instead each thread should update a unique atom $j$ (basically the $f_j$) and
+  computes the corresponding term on the right end side of
+
+  f_j += \sum_{k neighbor of j} f_kj.
+
+  In practice, we associate a unique thread block to each atom $j$ while the block
+  threads compute the f_kj (note the reverse order in the indices) individually.
+  The final sum (over the neighbors k of the atom j) is a block reduction that
+  can be done deterministically.
+*/
+
+template <typename FPTYPE, bool radial_only_ = true, int shared_memory_block_>
+__global__ void force_deriv_wrt_neighbors(
+    FPTYPE* force,
+    const FPTYPE* net_deriv,
+    const FPTYPE* in_deriv,
+    const int* nlist,  // neigbhor list
+    const int nframes,
+    const int nloc,  // number of atoms on this GPU
+    const int nall,  // total number of atoms
+    const int nnei)  // number fo neighbors
+{
+  // limited to 2 billions atoms and 2 billions frames
+  const int atom_id = blockIdx.x;
+  const int frame_id = blockIdx.z * gridDim.y + blockIdx.y;
+
+  if (frame_id >= nframes) {
     return;
   }
-  // deriv wrt neighbors
-  int j_idx = nlist[idx * nnei + idy];
-  if (j_idx < 0) {
-    return;
+
+  /*
+    structure of the arrays:
+       - nlist[] -> nlist[frame][atom][neighbor] (in multi dimensional array
+    form)
+       - net_deriv[] -> net_deriv[frame][atom][neighbor][1 or 4 elements]
+       - in_deriv[] -> in_deriv[frame][atom][neighbor][1 or 4 elements][xyz]
+       - force[] -> force[frame][all_atoms][xyz]
+  */
+  const int ndescrpt = nnei * ((radial_only_) ? (1) : (4));
+
+  // define various pointers for a specific frame.
+  const FPTYPE* frame_net_deriv_ =
+      &net_deriv[frame_id * nloc *
+                 ndescrpt];  // f_net_deriv[atom][neighbor][1 or 4]
+  const FPTYPE* frame_in_deriv_ =
+      &in_deriv[frame_id * nloc * ndescrpt *
+                3];  // f_in_deriv[atom][neighbor][1 or 4][3]
+  const int* frame_neighbor_list_ =
+      &nlist[frame_id * nnei * nloc];  // f_neighbor_list[atom][neighbors]
+  FPTYPE force_tmp[3] = {(FPTYPE)0., (FPTYPE)0., (FPTYPE)0.};
+
+  // used to broadcast if a given atom i has some of its neighbors on this GPU.
+  // In that case we may need to apply a final reduction at the end of the
+  // computation
+  //
+  // it is always set to 1 when we use one GPU only.
+  __shared__ int do_reduction;
+  if (threadIdx.x == 0) {
+    do_reduction = ((nall == nloc) ? (1) : (0));
   }
-  const int_64 kk = idx / nloc;  // frame index
-  atomicAdd(force + kk * nall * 3 + j_idx * 3 + idz,
-            net_deriv[idx * ndescrpt + idy] *
-                in_deriv[idx * ndescrpt * 3 + idy * 3 + idz]);
+  __syncthreads();
+
+  if (nloc != nall) {
+    /*
+      Each GPU contains a subpart of the entire system.
+
+      - The neighbor list only contains informations about the atoms located on
+      that GPU not the entire system.
+
+      - we do not have all needed information to do the computation so we have
+      to rely on the same logic than the original implementation using atomic
+      operations.
+
+      - To avoid the atomicAdd we treat each atom one by one and follow these
+      steps, (i) search if it is a neighbor of one or several local atoms (data
+      are on this GPU), (ii) if so calculate the forces and accumulate the
+      results locally (iii) when done apply a final reduction and store the
+      results back in global memory
+     */
+
+    for (int idx = threadIdx.x; idx < nloc * nnei; idx += blockDim.x) {
+      if (frame_neighbor_list_[idx] == atom_id) {
+        calculate_forces<FPTYPE, radial_only_>(force_tmp, frame_net_deriv_,
+                                               frame_in_deriv_, idx);
+
+        // the atomicAdd has NO impact on determinism. It is used to update a
+        // shared memory variable value across the thread block to indicate if
+        // we need to apply a block reduction at the end of the calculations.
+        // It will be different from zero if at least one thread has all
+        // informations for the computations. The value should simply be
+        // different from zero and updated once. The worst case scenario will
+        // be if all threads of a given block have to do the calculations.
+
+        if (do_reduction == 0) {
+          atomicAdd(&do_reduction, 1);
+        }
+        break;
+      }
+    }
+  } else {
+    /* Each GPU has the full information about the system. retrieving
+       information is easier as we only have to do a linear search over the
+       neighbor list of the neighbors not the entire neighbor list. */
+
+    /* each thread selects a neighbor. */
+    for (int neighbor_id = threadIdx.x; neighbor_id < nnei;
+         neighbor_id += blockDim.x) {
+      // collect all terms $\partial E_j / \partial D_{ji} \nabla_R_j D_{ji}$
+      // where the atom i is a neighbor of the atom j.
+      //
+      // Go through all neighbors of atom i, locate the position of
+      // the atom i in the neighbor list of the atom j and retrieve all
+      // necessary information.
+
+      const int atom_j = frame_neighbor_list_[atom_id * nnei + neighbor_id];
+
+      // The neighbors of a given atom are sorted by type and each resulting
+      // list is separated from the other by a series of -1. More details about
+      // the sorting can be found in https://doi.org/10.1016/j.cpc.2020.107624
+      //
+      // To illustrate this, take the neigbhors of a given atom of type a (in a
+      // system with two atoms type a and b) deepmd stores the neighbors as
+      //
+      // [neighbors list of type a], -1, -1, -1, ...., [neighbor list of type
+      // b], -1, -1, -1, .....
+
+      if (atom_j < 0) {
+        continue;
+      }
+
+      const int* nei_nei_list_ = &frame_neighbor_list_[atom_j * nnei];
+      int atom_id_position = 0;
+
+      // search the index of the atom i in the local neighbor list of atom j
+      for (atom_id_position = 0; atom_id_position < nnei; atom_id_position++) {
+        if (nei_nei_list_[atom_id_position] == atom_id) {
+          break;
+        }
+      }
+      calculate_forces<FPTYPE, radial_only_>(force_tmp, frame_net_deriv_,
+                                             frame_in_deriv_, atom_j * nnei + atom_id_position);
+    }
+  }
+
+  __syncthreads();
+
+  // Apply the final reduction.
+  if (do_reduction) {
+    __shared__ FPTYPE fx[shared_memory_block_];
+    __shared__ FPTYPE fy[shared_memory_block_];
+    __shared__ FPTYPE fz[shared_memory_block_];
+
+    fx[threadIdx.x] = force_tmp[0];
+    fy[threadIdx.x] = force_tmp[1];
+    fz[threadIdx.x] = force_tmp[2];
+    __syncthreads();
+
+    // do the final reduction
+    for (int tt = shared_memory_block_ / 2; tt > 0; tt >>= 1) {
+      if (threadIdx.x < tt) {
+        fx[threadIdx.x] += fx[threadIdx.x + tt];
+        fy[threadIdx.x] += fy[threadIdx.x + tt];
+        fz[threadIdx.x] += fz[threadIdx.x + tt];
+      }
+      __syncthreads();
+    }
+
+    /* Note the sign difference between the formula in the PRL paper and the
+       code. it is due to \nabla_R_j D_{ji} = -\nabla_R_i D_{ji} */
+    if (threadIdx.x == 0) {
+      const int64_t offset = (frame_id * nall + atom_id) * 3;
+      force[offset] += fx[0];
+      force[offset + 1] += fy[0];
+      force[offset + 2] += fz[0];
+    }
+  }
+}
+
+template <typename FPTYPE, bool radial_only_ = true>
+void prod_force_a_r_gpu(FPTYPE* force,
+                        const FPTYPE* net_deriv,
+                        const FPTYPE* in_deriv,
+                        const int* nlist,
+                        const int nloc,
+                        const int nall,
+                        const int nnei,
+                        const int nframes) {
+  DPErrcheck(gpuGetLastError());
+  DPErrcheck(gpuDeviceSynchronize());
+
+  const int ndescrpt = nnei * ((radial_only_) ? (1) : (4));
+  DPErrcheck(gpuMemset(force, 0, sizeof(FPTYPE) * nframes * nall * 3));
+
+  force_deriv_wrt_center_atom<FPTYPE, TPB><<<nframes * nloc, TPB>>>(
+      force, net_deriv, in_deriv, ndescrpt, nloc, nall);
+  DPErrcheck(gpuGetLastError());
+  DPErrcheck(gpuDeviceSynchronize());
+
+  dim3 block_grid;
+
+  if (nframes < 0xffff) {
+    block_grid = dim3(nall, nframes, 1);
+  } else {
+    const int sqrt_nframes = sqrt(nframes);
+    block_grid = dim3(nall, sqrt_nframes + 1, sqrt_nframes + 1);
+  }
+  // to accomodate AMD GPU
+  dim3 thread_grid(64, 1, 1);
+  force_deriv_wrt_neighbors<FPTYPE, radial_only_, 64>
+      <<<block_grid, thread_grid>>>(force, net_deriv, in_deriv, nlist, nframes,
+                                    nloc, nall, nnei);
+  DPErrcheck(gpuGetLastError());
+  DPErrcheck(gpuDeviceSynchronize());
 }
 
 namespace deepmd {
@@ -110,24 +293,8 @@ void prod_force_a_gpu(FPTYPE* force,
                       const int nall,
                       const int nnei,
                       const int nframes) {
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-  const int ndescrpt = nnei * 4;
-  DPErrcheck(gpuMemset(force, 0, sizeof(FPTYPE) * nframes * nall * 3));
-
-  force_deriv_wrt_center_atom<FPTYPE, TPB><<<nframes * nloc, TPB>>>(
-      force, net_deriv, in_deriv, ndescrpt, nloc, nall);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-
-  const int LEN = 64;
-  const int nblock = (nnei + LEN - 1) / LEN;
-  dim3 block_grid(nframes * nloc, nblock);
-  dim3 thread_grid(LEN, 3);
-  force_deriv_wrt_neighbors_a<<<block_grid, thread_grid>>>(
-      force, net_deriv, in_deriv, nlist, nloc, nall, nnei);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
+  prod_force_a_r_gpu<FPTYPE, false>(force, net_deriv, in_deriv, nlist, nloc,
+                                    nall, nnei, nframes);
 }
 
 template <typename FPTYPE>
@@ -139,24 +306,8 @@ void prod_force_r_gpu(FPTYPE* force,
                       const int nall,
                       const int nnei,
                       const int nframes) {
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-  const int ndescrpt = nnei * 1;
-  DPErrcheck(gpuMemset(force, 0, sizeof(FPTYPE) * nframes * nall * 3));
-
-  force_deriv_wrt_center_atom<FPTYPE, TPB><<<nframes * nloc, TPB>>>(
-      force, net_deriv, in_deriv, ndescrpt, nloc, nall);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-
-  const int LEN = 64;
-  const int nblock = (nnei + LEN - 1) / LEN;
-  dim3 block_grid(nframes * nloc, nblock);
-  dim3 thread_grid(LEN, 3);
-  force_deriv_wrt_neighbors_r<<<block_grid, thread_grid>>>(
-      force, net_deriv, in_deriv, nlist, nloc, nall, nnei);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
+  prod_force_a_r_gpu<FPTYPE, true>(force, net_deriv, in_deriv, nlist, nloc,
+                                   nall, nnei, nframes);
 }
 
 template void prod_force_a_gpu<float>(float* force,
--- source/lib/src/gpu/prod_virial.cu
+++ source/lib/src/gpu/prod_virial.cu
@@ -26,82 +26,174 @@ __global__ void atom_virial_reduction(FPTYPE* virial,
   }
 }
 
-template <typename FPTYPE>
-__global__ void virial_deriv_wrt_neighbors_a(FPTYPE* virial,
-                                             FPTYPE* atom_virial,
-                                             const FPTYPE* net_deriv,
-                                             const FPTYPE* in_deriv,
-                                             const FPTYPE* rij,
-                                             const int* nlist,
-                                             const int nloc,
-                                             const int nnei) {
-  // idx -> nloc
-  // idy -> nnei
-  // idz = dd0 * 3 + dd1
-  // dd0 = idz / 3
-  // dd1 = idz % 3
-  const int_64 idx = blockIdx.x;
-  const unsigned int idy = blockIdx.y * blockDim.x + threadIdx.x;
-  const unsigned int idz = threadIdx.y;
-  const int ndescrpt = nnei * 4;
-  if (idy >= nnei) {
-    return;
+template <typename FPTYPE, bool radial_only_>
+__inline__ __device__ void calculate_virial(
+    FPTYPE* __restrict__ virial_tmp__,
+    const FPTYPE* __restrict__ net_deriv__,
+    const FPTYPE* __restrict__ in_deriv__,
+    const FPTYPE* __restrict__ rij__,
+    const int idx) {
+  /* take the rij from the atom i NOT the atom j as the coordinates system is
+     locally dependent */
+  const FPTYPE rij_[3] = {rij__[idx * 3], rij__[idx * 3 + 1],
+                          rij__[idx * 3 + 2]};
+  const int offset_j = idx * ((radial_only_) ? (1) : (4));
+
+  for (int idw = 0; idw < ((radial_only_) ? (1) : (4)); idw++) {
+    const FPTYPE cnd = net_deriv__[offset_j + idw];
+    const FPTYPE in_der[3] = {in_deriv__[(offset_j + idw) * 3 + 0],
+                              in_deriv__[(offset_j + idw) * 3 + 1],
+                              in_deriv__[(offset_j + idw) * 3 + 2]};
+
+    virial_tmp__[0] += cnd * rij_[0] * in_der[0];
+    virial_tmp__[1] += cnd * rij_[1] * in_der[0];
+    virial_tmp__[2] += cnd * rij_[2] * in_der[0];
+    virial_tmp__[3] += cnd * rij_[0] * in_der[1];
+    virial_tmp__[4] += cnd * rij_[1] * in_der[1];
+    virial_tmp__[5] += cnd * rij_[2] * in_der[1];
+    virial_tmp__[6] += cnd * rij_[0] * in_der[2];
+    virial_tmp__[7] += cnd * rij_[1] * in_der[2];
+    virial_tmp__[8] += cnd * rij_[2] * in_der[2];
   }
-  int j_idx = nlist[idx * nnei + idy];
-  if (j_idx < 0) {
-    return;
-  }
-  // atomicAdd(
-  //    virial + idz,
-  //    net_deriv[idx * ndescrpt + idy * 4 + idw] * rij[idx * nnei * 3 + idy * 3
-  //    + idz / 3] * in_deriv[idx * ndescrpt * 3 + (idy * 4 + idw) * 3 + idz %
-  //    3]);
-  FPTYPE virial_tmp = (FPTYPE)0.;
-  for (int idw = 0; idw < 4; ++idw) {
-    virial_tmp += net_deriv[idx * ndescrpt + idy * 4 + idw] *
-                  rij[idx * nnei * 3 + idy * 3 + idz % 3] *
-                  in_deriv[idx * ndescrpt * 3 + (idy * 4 + idw) * 3 + idz / 3];
-  }
-  atomicAdd(atom_virial + j_idx * 9 + idz, virial_tmp);
 }
 
-template <typename FPTYPE>
-__global__ void virial_deriv_wrt_neighbors_r(FPTYPE* virial,
-                                             FPTYPE* atom_virial,
-                                             const FPTYPE* net_deriv,
-                                             const FPTYPE* in_deriv,
-                                             const FPTYPE* rij,
-                                             const int* nlist,
-                                             const int nloc,
-                                             const int nnei) {
-  // idx -> nloc
-  // idy -> nnei
-  // idz = dd0 * 3 + dd1
-  // dd0 = idz / 3
-  // dd1 = idz % 3
-  const int_64 idx = blockIdx.x;
-  const unsigned int idy = blockIdx.y * blockDim.x + threadIdx.x;
-  const unsigned int idz = threadIdx.y;
-  const int ndescrpt = nnei * 1;
+template <typename FPTYPE, bool radial_only_, int shared_mem_block_size_>
+__global__ void virial_deriv_wrt_neighbors(FPTYPE* __restrict__ virial,
+                                           FPTYPE* __restrict__ atom_virial,
+                                           const FPTYPE* __restrict__ net_deriv,
+                                           const FPTYPE* __restrict__ in_deriv,
+                                           const FPTYPE* __restrict__ rij,
+                                           const int* __restrict__ nlist,
+                                           const int nloc,
+                                           const int nall,
+                                           const int nnei) {
+  const int atom_id = blockIdx.x;
+  const int frame_id = 0;
+  const int ndescrpt = nnei * ((radial_only_) ? (1) : (4));
 
-  if (idy >= nnei) {
-    return;
+  // define various pointers to for a  specific frame.
+  const FPTYPE* frame_net_deriv_ = &net_deriv[frame_id * nloc * ndescrpt];
+  const FPTYPE* frame_in_deriv_ = &in_deriv[frame_id * nloc * ndescrpt * 3];
+  const FPTYPE* frame_rij_ = &rij[frame_id * nloc * nnei * 3];
+  const int* frame_neighbor_list_ = &nlist[frame_id * nloc * nnei];
+
+  FPTYPE virial_tmp[9];
+  memset(virial_tmp, 0, sizeof(FPTYPE) * 9);
+  __shared__ int do_reduction;
+
+  // it is set to one if all atoms fit on one GPU
+  if (threadIdx.x == 0) {
+    do_reduction = ((nall == nloc) ? (1) : (0));
   }
-  int j_idx = nlist[idx * nnei + idy];
-  if (j_idx < 0) {
-    return;
+  __syncthreads();
+
+  if (nloc != nall) {
+    /*
+      We want to compute the original formula to avoid the atomic operation
+      over multiple thread blocks. The neighbor list does not allow that easily
+      in the multi-gpu case that's why we have this complicated and expensive
+      search. It can be improved at the expense of more memory.
+    */
+    for (int idx = threadIdx.x; idx < nloc * nnei; idx += blockDim.x) {
+      if (frame_neighbor_list_[idx] == atom_id) {
+        calculate_virial<FPTYPE, radial_only_>(
+            virial_tmp, frame_net_deriv_, frame_in_deriv_, frame_rij_, idx);
+
+        // the atomicAdd has NO impact on determinism. It is used to update a
+        // shared memory variable value across the thread block to indicate if
+        // we need to apply a block reduction at the end of the calculations.
+        // It will be different from zero if at least one thread has all
+        // informations for the computations. The value should simply be
+        // different from zero and updated once. The worst case scenario will
+        // be if all threads of a given block have to do the calculations.
+
+        if (do_reduction == 0) {
+          atomicAdd(&do_reduction, 1);
+        }
+        break;
+      }
+    }
+  } else {
+    for (int neighbor_id = threadIdx.x; neighbor_id < nnei;
+         neighbor_id += blockDim.x) {
+      // Go through all neighbors of atom i, locate the position of the atom i
+      // in the neighbor list of the atom j and retrieve all necessary
+      // information.
+
+      const int atom_j = frame_neighbor_list_[atom_id * nnei + neighbor_id];
+
+      if (atom_j < 0) {
+        continue;
+      }
+
+      const int* nei_nei_list_ = &frame_neighbor_list_[atom_j * nnei];
+      int atom_id_position = 0;
+      /* search for the atom i position in the neighbors list of atom_j */
+      for (atom_id_position = 0; atom_id_position < nnei; atom_id_position++) {
+        if (nei_nei_list_[atom_id_position] == atom_id) {
+          // add the contribution of atom j to the local virial term of atom i.
+          calculate_virial<FPTYPE, radial_only_>(
+              virial_tmp, frame_net_deriv_, frame_in_deriv_, frame_rij_,
+              atom_j * nnei + atom_id_position);
+          break;
+        }
+      }
+    }
   }
-  // atomicAdd(
-  //    virial + idz,
-  //    net_deriv[idx * ndescrpt + idy * 4 + idw] * rij[idx * nnei * 3 + idy * 3
-  //    + idz / 3] * in_deriv[idx * ndescrpt * 3 + (idy * 4 + idw) * 3 + idz %
-  //    3]);
-  atomicAdd(atom_virial + j_idx * 9 + idz,
-            net_deriv[idx * ndescrpt + idy] *
-                rij[idx * nnei * 3 + idy * 3 + idz % 3] *
-                in_deriv[idx * ndescrpt * 3 + idy * 3 + idz / 3]);
+  __syncthreads();
+
+  if (do_reduction) {
+    __shared__ FPTYPE vab[shared_mem_block_size_];
+
+    /* do the reduction over the thread block component by component */
+    for (int i = 0; i < 9; i++) {
+      vab[threadIdx.x] = virial_tmp[i];
+      __syncthreads();
+
+      for (int tt = blockDim.x / 2; tt > 0; tt >>= 1) {
+        if (threadIdx.x < tt) {
+          vab[threadIdx.x] += vab[threadIdx.x + tt];
+        }
+        __syncthreads();
+      }
+      virial_tmp[i] = vab[0];
+    }
+
+    if (threadIdx.x < 9) {
+      atom_virial[9 * atom_id + threadIdx.x] = virial_tmp[threadIdx.x];
+    }
+  }
 }
 
+template <typename FPTYPE, bool radial_only_>
+void prod_virial_a_r_gpu(FPTYPE* virial,
+                         FPTYPE* atom_virial,
+                         const FPTYPE* net_deriv,
+                         const FPTYPE* in_deriv,
+                         const FPTYPE* rij,
+                         const int* nlist,
+                         const int nloc,
+                         const int nall,
+                         const int nnei) {
+  DPErrcheck(gpuGetLastError());
+  DPErrcheck(gpuDeviceSynchronize());
+  DPErrcheck(gpuMemset(virial, 0, sizeof(FPTYPE) * 9));
+  DPErrcheck(gpuMemset(atom_virial, 0, sizeof(FPTYPE) * 9 * nall));
+
+  dim3 block_grid = dim3(nall, 1, 1);
+  // to accomodate AMD warp size
+  dim3 thread_grid = dim3(64, 1, 1);
+  virial_deriv_wrt_neighbors<FPTYPE, radial_only_, 64>
+      <<<block_grid, thread_grid>>>(virial, atom_virial, net_deriv, in_deriv,
+                                    rij, nlist, nloc, nall, nnei);
+  DPErrcheck(gpuGetLastError());
+  DPErrcheck(gpuDeviceSynchronize());
+
+  // reduction atom_virial to virial
+  atom_virial_reduction<FPTYPE, TPB><<<9, TPB>>>(virial, atom_virial, nall);
+  DPErrcheck(gpuGetLastError());
+  DPErrcheck(gpuDeviceSynchronize());
+}
 namespace deepmd {
 template <typename FPTYPE>
 void prod_virial_a_gpu(FPTYPE* virial,
@@ -113,24 +205,8 @@ void prod_virial_a_gpu(FPTYPE* virial,
                        const int nloc,
                        const int nall,
                        const int nnei) {
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-  DPErrcheck(gpuMemset(virial, 0, sizeof(FPTYPE) * 9));
-  DPErrcheck(gpuMemset(atom_virial, 0, sizeof(FPTYPE) * 9 * nall));
-
-  const int LEN = 16;
-  int nblock = (nnei + LEN - 1) / LEN;
-  dim3 block_grid(nloc, nblock);
-  dim3 thread_grid(LEN, 9);
-  // compute virial of a frame
-  virial_deriv_wrt_neighbors_a<<<block_grid, thread_grid>>>(
-      virial, atom_virial, net_deriv, in_deriv, rij, nlist, nloc, nnei);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-  // reduction atom_virial to virial
-  atom_virial_reduction<FPTYPE, TPB><<<9, TPB>>>(virial, atom_virial, nall);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
+  prod_virial_a_r_gpu<FPTYPE, false>(virial, atom_virial, net_deriv, in_deriv,
+                                     rij, nlist, nloc, nall, nnei);
 }
 
 template <typename FPTYPE>
@@ -143,24 +219,8 @@ void prod_virial_r_gpu(FPTYPE* virial,
                        const int nloc,
                        const int nall,
                        const int nnei) {
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-  DPErrcheck(gpuMemset(virial, 0, sizeof(FPTYPE) * 9));
-  DPErrcheck(gpuMemset(atom_virial, 0, sizeof(FPTYPE) * 9 * nall));
-
-  const int LEN = 16;
-  int nblock = (nnei + LEN - 1) / LEN;
-  dim3 block_grid(nloc, nblock);
-  dim3 thread_grid(LEN, 9);
-  // compute virial of a frame
-  virial_deriv_wrt_neighbors_r<<<block_grid, thread_grid>>>(
-      virial, atom_virial, net_deriv, in_deriv, rij, nlist, nloc, nnei);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
-  // reduction atom_virial to virial
-  atom_virial_reduction<FPTYPE, TPB><<<9, TPB>>>(virial, atom_virial, nall);
-  DPErrcheck(gpuGetLastError());
-  DPErrcheck(gpuDeviceSynchronize());
+  prod_virial_a_r_gpu<FPTYPE, true>(virial, atom_virial, net_deriv, in_deriv,
+                                    rij, nlist, nloc, nall, nnei);
 }
 
 template void prod_virial_a_gpu<float>(float* virial,
